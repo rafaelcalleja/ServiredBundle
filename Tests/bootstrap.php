99a46471469dffce7504825acb96379c168e733d
Rafael Calleja <rafaelcalleja@gmail.com>
2013-11-16 21:54:05 +0100

    añadido bootstrap doctrine annotations

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -20,4 +20,5 @@ AnnotationRegistry::registerLoader(function($class) use ($loader) {
     return class_exists($class, false);
 });
 
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 

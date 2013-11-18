5325fd827c820dd139100aa8603be7297bdae975
Rafael Calleja <rafaelcalleja@gmail.com>
2013-11-18 02:08:52 +0100

    añadido twig render para tests del controller

--- Tests/Resources/app/AppKernel.php
+++ Tests/Resources/app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
     {
         return array(
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
             //new \Symfony\Bundle\MonologBundle\MonologBundle(),
             new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
 	    	new \RC\ServiredBundle\RCServiredBundle(),
